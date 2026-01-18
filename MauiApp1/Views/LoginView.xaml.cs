c3106b01f54fc912cf80e65740e544382834fc9d
Nenad Popović <np20240191@student.fon.bg.ac.rs>
2026-01-18 02:52:29 +0100

    aa

--- MauiApp1/Views/LoginView.xaml.cs
+++ MauiApp1/Views/LoginView.xaml.cs
@@ -4,9 +4,14 @@ namespace MauiApp1.Views
 {
     public partial class LoginView : ContentPage
     {
+        private async void OnGoToPage2(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new HomeView());
+        }
         public LoginView()
         {
             InitializeComponent();
+            
             BindingContext = new LoginViewModel();
         }
     }
